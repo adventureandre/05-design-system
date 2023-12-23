481a18815281c6281422a90be5c28562fb1f6bb1
Andre Luiz <adventureandre@hotmail.com>
2023-12-23 18:09:33 -0300

    intalando o storybook e configurando

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,6 +1,7 @@
-import { colors } from '@ignite-ui/tokens';
+import { styled } from './styles'
 
-
-export function App(){
-    return <h1 style={{color: colors.ignite300}}>Hello Word</h1>
-}
+export const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite500',
+  borderRadius: '$md',
+})
