e4400ffb5913e9eee68803b3f9e61b366c95a816
Andre Luiz <adventureandre@hotmail.com>
2023-12-24 13:23:58 -0300

    Design System em React - Story: Button

--- packages/docs/src/stories/Buttom.stories.tsx
+++ packages/docs/src/stories/Buttom.stories.tsx
@@ -1,16 +1,14 @@
-import { Button } from '@ignite-ui/react'
+import { Button, ButtonProps } from '@ignite-ui/react'
 import { Meta, StoryObj } from '@storybook/react'
 
 export default {
   title: 'Button',
   component: Button,
-} as Meta
-
-export const Primary: StoryObj = {
   args: { children: 'enviar' },
-}
-export const Seconday: StoryObj = {
-  args: {
-    children: 'Test',
-  },
+} as Meta<ButtonProps>
+
+export const Primary: StoryObj<ButtonProps> = {}
+
+export const Big: StoryObj<ButtonProps> = {
+  args: { size: 'small' },
 }
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,8 +1,34 @@
+import { ComponentProps } from 'react'
 import { styled } from './styles'
 
 export const Button = styled('button', {
   fontFamily: '$default',
   backgroundColor: '$ignite300',
-  borderRadius: '$md',
+  borderRadius: '$small',
   border: 0,
+  fontWeight: '$bold',
+  color: 'White',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
+
+  '&:hover': {
+    backgroundColor: '$ignite500',
+  },
 })
+
+export type ButtonProps = ComponentProps<typeof Button>
